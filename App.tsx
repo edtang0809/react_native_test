cd99cc9242ffb8d9ea67bb3850f4ab05d7d14f2d
edtang0809 <edtang0809@gmail.com>
2024-06-11 15:39:01 +0800

    first commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * 
+ * @format
+ */
+
+import React from 'react';
+import Flex1 from './components/Flex1';
+
+
+export default function App(){
+  return(
+      <Flex1/>
+  )
+};
---
+++ components/Flex1.js
@@ -0,0 +1,36 @@
+import React,{useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function Flex1() {
+
+    const [name,setName] = useState('avon')
+    const [count,setCount] = useState(0)
+
+    const changeName = () =>{
+        setName('Ed')
+        console.log('按到changeName按鈕了')
+    }
+
+    const changeCount = () =>{
+        setCount(count+1)
+        console.log('按到changeCount按鈕了')
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on {name} app!</Text>
+            <Text>計數器現在是{count}個</Text>
+            <Button title='change name' onPress={()=>changeName()}/>
+            <Button title='change count' onPress={()=>changeCount()}/>
+        </View>
+    );
+    }
+const styles = StyleSheet.create({
+    container: {
+        flexDirection:'column',
+        flex:1,
+        backgroundColor:'pink',
+        alignItems:'center',//子項目對齊方式(次軸)，指的是<Text>的部分
+        justifyContent:'space-around',//子項目對齊方式(主軸)，指的是<Text>的部分
+    },
+});
